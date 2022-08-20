197ec0c2d707bd33fb066291cced1b8887d6e207
康颜 <564469630@qq.com>
2022-08-20 11:32:32 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js");
+}
+
+// 推送消息
+const handleMessage = () => {
+  new Notification("这是一条测试通知消息", {
+    body: "锅巴🌶，喷火！🔥",
+    icon: "assets/guoba.jpeg",
+  });
+};
+
+// 用户授权
+const handleAuth = () => {
+  Notification.requestPermission().then((value) => {
+    if (value !== "granted") {
+      alert("请授权！");
+      return;
+    }
+    handleMessage();
+  });
+};
+
+const addBtn = document.getElementById("desktop");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  // 防止 Chrome 67 及更早版本自动显示安装提示
+  e.preventDefault();
+  // 稍后再触发此事件
+  deferredPrompt = e;
+  // 更新 UI 以提醒用户可以将 App 安装到桌面
+  addBtn.style.display = "block";
+
+  addBtn.addEventListener("click", () => {
+    // 隐藏显示 A2HS 按钮的界面
+    addBtn.style.display = "none";
+    // 显示安装提示
+    deferredPrompt.prompt();
+    // 等待用户反馈
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("用户同意添加");
+      } else {
+        console.log("用户拒绝添加");
+      }
+      deferredPrompt = null;
+    });
+  });
+});
---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const cacheName = "pwa-v1";
+
+const contentToCache = [
+  "/",
+  "index.html",
+  "app.js",
+  "assets/favicon.ico",
+  "assets/guoba.jpeg",
+];
+
+// 初始化
+self.addEventListener("install", (e) => {
+  console.log("SW 初始化");
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("SW 缓存");
+      return cache.addAll(contentToCache);
+    })
+  );
+});
+
+// 拦截请求 加载与缓存
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      console.log(`SW 加载资源: ${e.request.url}`);
+      return (
+        r ||
+        fetch(e.request).then((response) => {
+          return caches.open(cacheName).then((cache) => {
+            console.log(`SW 缓存新资源: ${e.request.url}`);
+            if (e.request.url.startsWith("http")) {
+              cache.put(e.request, response.clone());
+            }
+            return response;
+          });
+        })
+      );
+    })
+  );
+});
+
+// 激活 删除旧缓存
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (cacheName.indexOf(key) === -1) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
